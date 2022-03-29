c96ce944e273d5927d06822f77e4b777069638fe
Kirill Usichenko <kirillusichenko@gmail.com>
2022-03-29 04:28:14 +0300

    fix type error

--- pages/features.tsx
+++ pages/features.tsx
@@ -3,11 +3,11 @@ import { NextPage } from 'next/types';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 
 import { Main } from '../components/Main/styles';
-import { Container } from '../components/common/Container/Container';
 import Header from '../components/common/Header/Header';
 import Preview from '../components/Features/Preview';
 import Supercharge from '../components/Features/Supercharge';
 import Footer from '../components/common/Footer';
+import { Container } from '../components/common/Container/Container';
 
 export async function getStaticProps({ locale }: any) {
 	return {
