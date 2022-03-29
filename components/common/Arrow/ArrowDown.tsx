e1db785ef100b5e42e65603c8dc22d7e2e425afc
Kirill Usichenko <kirillusichenko@gmail.com>
2022-03-29 04:32:49 +0300

    fix type error

--- components/common/Arrow/ArrowDown.tsx
+++ components/common/Arrow/ArrowDown.tsx
@@ -1,8 +1,6 @@
 import React, { FunctionComponent } from 'react';
 
-type PropsType = {};
-
-const ArrowDown: FunctionComponent<PropsType> = () => {
+const ArrowDown: FunctionComponent<any> = () => {
 	return (
 		<>
 			<svg width='10' height='6' viewBox='0 0 10 6' fill='none' xmlns='http://www.w3.org/2000/svg'>
