efc5bbc3d39fb994820c5c6b0579c452fed74b3f
Kirill Usichenko <kirillusichenko@gmail.com>
2022-04-04 15:16:34 +0300

    fix: imports

--- pages/company.tsx
+++ pages/company.tsx
@@ -14,6 +14,7 @@ import Role from '../components/company/Role/Role';
 import Contact from '../components/company/Contact/Contact';
 import { Main } from '../components/main/Styles';
 
+
 export async function getStaticProps({ locale }: any) {
 	return {
 		props: {
