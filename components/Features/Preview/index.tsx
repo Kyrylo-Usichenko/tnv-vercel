e551d6d4d6b7e20110a2f56d5814fa94bfc439ba
Kirill Usichenko <kirillusichenko@gmail.com>
2022-04-28 16:41:03 +0300

    fix: remove shadow, add image with shadow, preview bloc, preview, features page

--- components/Features/Preview/index.tsx
+++ components/Features/Preview/index.tsx
@@ -124,13 +124,20 @@ const Preview: FC<PreviewProps> = ({ openModal }) => {
 								ref={mainImg}
 							/>
 						</picture>
+						{/*<AppImg*/}
+						{/*	src='/images/features/preview/app.png'*/}
+						{/*	srcSet='/images/features/preview/app@2x.png 2x'*/}
+						{/*	alt='message'*/}
+						{/*	animate={animate}*/}
+						{/*/>*/}
+
 						<AppImg
-							src='/images/features/preview/app.png'
-							srcSet='/images/features/preview/app@2x.png 2x'
+							src='/images/features/preview/app-shadow.png'
+							srcSet='/images/features/preview/app-shadow@2x.png 2x'
 							alt='message'
 							animate={animate}
 						/>
-						<AppShadow animate={animate} />
+						{/*<AppShadow animate={animate} />*/}
 						<RequestImg
 							src='/images/features/preview/request.png'
 							srcSet='/images/features/preview/request@2x.png 2x'
@@ -345,11 +352,11 @@ const PreviewImg = styled.div`
 
 const AppImg = styled.img<{ animate: boolean }>`
 	position: absolute;
-	right: -2%;
-	bottom: -22%;
+	right: -13%;
+	bottom: -33%;
 	z-index: 1;
 	border-radius: 0;
-	width: 58.6%;
+	width: 72.6%;
 	height: auto;
 	opacity: 0;
 	animation: ${({ animate }) => (animate ? 'appImgAnim 0.5s ease-in' : 'none')};
@@ -366,66 +373,68 @@ const AppImg = styled.img<{ animate: boolean }>`
 	}
 
 	@media (min-width: 768px) {
-		width: 39.2%;
+		width: 50.2%;
 		height: auto;
-		left: 4%;
-		top: 6%;
+		left: 1%;
+		top: 1%;
 	}
 
 	@media (min-width: 1024px) {
-		width: 288px;
-		left: 32px;
-		top: 32px;
+		width: 362px;
+		left: 15px;
+		top: 8px;
 	}
 
 	@media (min-width: 1440px) {
-		left: 48px;
-		top: 48px;
+		left: 29px;
+		top: 25px;
+		width: 362px;
 	}
 `;
-
-const AppShadow = styled.div<{ animate: boolean }>`
-	position: absolute;
-	width: 52%;
-	height: 48%;
-	background: rgba(0, 0, 0, 0.1);
-	filter: blur(16px);
-	border-radius: 6px;
-	right: -4%;
-	bottom: -23%;
-	opacity: 0;
-	animation: ${({ animate }) => (animate ? 'appImgAnim 0.5s ease-in' : 'none')};
-	animation-fill-mode: forwards;
-	animation-delay: 3.7s;
-
-	@keyframes appImgAnim {
-		from {
-			opacity: 0;
-		}
-		to {
-			opacity: 1;
-		}
-	}
-
-	@media (min-width: 768px) {
-		width: 34.4%;
-		height: 49.5%;
-		left: 11%;
-		top: 13%;
-	}
-
-	@media (min-width: 1024px) {
-		width: 253px;
-		height: 273px;
-		left: 80px;
-		top: 75px;
-	}
-
-	@media (min-width: 1440px) {
-		left: 95px;
-		top: 90px;
-	}
-`;
+// const AppShadow = styled.div<{ animate: boolean }>`
+// 	position: absolute;
+// 	width: 52%;
+// 	height: 48%;
+// 	background: rgba(0, 0, 0, 0.1);
+//
+// 	filter: blur(16px);
+// 	-webkit-filter: blur(16px);
+// 	border-radius: 6px;
+// 	right: -4%;
+// 	bottom: -23%;
+// 	opacity: 0;
+// 	animation: ${({ animate }) => (animate ? 'appImgAnim 0.5s ease-in' : 'none')};
+// 	animation-fill-mode: forwards;
+// 	animation-delay: 3.7s;
+//
+// 	@keyframes appImgAnim {
+// 		from {
+// 			opacity: 0;
+// 		}
+// 		to {
+// 			opacity: 1;
+// 		}
+// 	}
+//
+// 	@media (min-width: 768px) {
+// 		width: 34.4%;
+// 		height: 49.5%;
+// 		left: 11%;
+// 		top: 13%;
+// 	}
+//
+// 	@media (min-width: 1024px) {
+// 		width: 253px;
+// 		height: 273px;
+// 		left: 80px;
+// 		top: 75px;
+// 	}
+//
+// 	@media (min-width: 1440px) {
+// 		left: 95px;
+// 		top: 90px;
+// 	}
+// `;
 
 const RequestImg = styled.img<{ animate: boolean }>`
 	position: absolute;
